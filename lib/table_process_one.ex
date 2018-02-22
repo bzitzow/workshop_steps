ae665053d2f196262658d7fc05d2759256854b5a
Brian Zitzow <bzitzow@wsgc.com>
2018-02-22 08:42:41 -0800

    Workshop solution, passing test.

--- lib/table_process_one.ex
+++ lib/table_process_one.ex
@@ -1,11 +1,18 @@
 defmodule TableProcessOne do
+  import IO
+
   def ping do
     receive do
+      :ping -> puts ":ping received"
       # insert receive here
     end
   end
 
   def start do
     # insert spawn here
+    # spawn(fn () -> "" end)
+    # spawn(TableProcessOne, :ping, [])
+    spawn(&TableProcessOne.ping/0)
+    # spawn(fn () -> ping() end)
   end
 end
